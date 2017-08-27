4ae58712eb1f2edf7c4d7d698d6d94a449b83834
Andrew Ippoliti <acippolit@gmail.com>
2017-08-27 16:36:17 -0400

    Updated the app so that it will return a generated csv on the results.csv url

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import json
 import random
-# import urllib.parse
+import StringIO
+import csv
 
 import requests
 from bs4 import BeautifulSoup
 
-from flask import Flask, render_template, url_for, redirect, request, Markup, flash, session, request
+from flask import Flask
+from flask import render_template
+from flask import make_response
 
 app = Flask(__name__)
 
@@ -14,12 +17,27 @@ def hello():
 	# return "Hello World!"
 	return render_template("index.html")
 
-@app.route("/result", methods=['GET', 'POST'])
+@app.route("/results.csv", methods=['GET', 'POST'])
 def result():
-	print('we ran the dunction')
-	return render_template("result.html")
-
-
+	si = StringIO.StringIO()
+	cw = csv.writer(si)
+	
+	# TODO: replace csvList with the actual random list
+	csvList = [
+		'a b c d e f'.split(),
+		'1 2 3 4 5 6'.split(),
+		'1 2 3 4 5 6'.split(),
+		'1 2 3 4 5 6'.split(),
+		'1 2 3 4 5 6'.split(),
+		'1 2 3 4 5 6'.split()
+	]
+	# ------------------------
+	
+	cw.writerows(csvList)
+	output = make_response(si.getvalue())
+	output.headers["Content-Disposition"] = "attachment; filename=export.csv"
+	output.headers["Content-type"] = "text/csv"
+	return output
 
 if __name__ == "__main__":
-    app.run(debug =True)
+	app.run(host='0.0.0.0',debug =True)
